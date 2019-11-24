92e154a9a50fa611a0f99b793b719bf97dc21542
Raeleus <raymond.ray3k@gmail.com>
2019-11-24 02:39:48 -0800

    Added clear functionality to Scene Composer

--- core/src/com/ray3k/skincomposer/dialog/scenecomposer/undoables/MenuClearUndoable.java
+++ core/src/com/ray3k/skincomposer/dialog/scenecomposer/undoables/MenuClearUndoable.java
@@ -1,14 +1,35 @@
 package com.ray3k.skincomposer.dialog.scenecomposer.undoables;
 
+import com.ray3k.skincomposer.dialog.scenecomposer.DialogSceneComposer;
+import com.ray3k.skincomposer.dialog.scenecomposer.DialogSceneComposerModel;
+
 public class MenuClearUndoable implements SceneComposerUndoable {
+    private DialogSceneComposer dialog;
+    private DialogSceneComposerModel.SimGroup group;
+    private DialogSceneComposerModel.SimGroup previousGroup;
+    
+    public MenuClearUndoable() {
+        dialog = DialogSceneComposer.dialog;
+        previousGroup = dialog.model.root;
+        group = new DialogSceneComposerModel.SimGroup();
+    }
+    
     @Override
     public void undo() {
+        dialog.model.root = previousGroup;
     
+        dialog.simActor = previousGroup;
+        dialog.populateProperties();
+        dialog.populatePath();
     }
     
     @Override
     public void redo() {
-    
+        dialog.model.root = group;
+        
+        dialog.simActor = group;
+        dialog.populateProperties();
+        dialog.populatePath();
     }
     
     @Override
