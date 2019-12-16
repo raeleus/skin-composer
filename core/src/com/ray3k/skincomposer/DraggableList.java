a77ca5225285ff172b626d50fc1f10ba11bd0137
Raeleus <raymond.ray3k@gmail.com>
2019-12-16 04:31:44 -0800

    Added DraggableList and DraggableTextList

---
+++ core/src/com/ray3k/skincomposer/DraggableList.java
@@ -0,0 +1,333 @@
+package com.ray3k.skincomposer;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop.Payload;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop.Source;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop.Target;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ObjectMap;
+
+public class DraggableList extends WidgetGroup {
+    private DraggableListStyle style;
+    private Table table;
+    private Array<Actor> actors;
+    private ObjectMap<Actor, Actor> dragActors;
+    private ObjectMap<Actor, Actor> validDragActors;
+    private ObjectMap<Actor, Actor> invalidDragActors;
+    private DragAndDrop dragAndDrop;
+    private ButtonStyle dividerStyle;
+    private Array<Button> dividers;
+    private boolean vertical;
+    
+    public DraggableList(boolean vertical, Skin skin) {
+        this(vertical, skin, vertical ? "default-vertical" : "default-horizontal");
+    }
+    
+    public DraggableList(boolean vertical, Skin skin, String style) {
+        this(vertical, skin.get(style, DraggableListStyle.class));
+    }
+    
+    public DraggableList(boolean vertical, DraggableListStyle style) {
+        this.vertical = vertical;
+        this.style = style;
+        
+        dragAndDrop = new DragAndDrop();
+        
+        dividerStyle = new ButtonStyle();
+        dividerStyle.up = style.dividerUp;
+        dividerStyle.over = style.dividerOver;
+        dividerStyle.checked = style.dividerOver;
+        
+        dividers = new Array<>();
+        dragActors = new ObjectMap<>();
+        validDragActors = new ObjectMap<>();
+        invalidDragActors = new ObjectMap<>();
+        
+        table = new Table();
+        table.setBackground(style.background);
+        addActor(table);
+        
+        actors = new Array<>();
+    }
+    
+    public void add(Actor actor) {
+        add(actor, null, null, null);
+    }
+    
+    public void add(Actor actor, Actor dragActor, Actor validDragActor, Actor invalidDragActor) {
+        actors.add(actor);
+        dragActors.put(actor, dragActor);
+        validDragActors.put(actor, validDragActor);
+        invalidDragActors.put(actor, invalidDragActor);
+        updateTable();
+    }
+    
+    public void addAll(Array<Actor> actors) {
+        addAll(actors, null, null, null);
+    }
+    
+    public void addAll(Array<Actor> actors, Array<Actor> dragActors, Array<Actor> validDragActors, Array<Actor> invalidDragActors) {
+        for (int i = 0; i < actors.size; i++) {
+            Actor actor = actors.get(i);
+            Actor dragActor = null;
+            Actor validDragActor = null;
+            Actor invalidDragActor = null;
+            
+            if (dragActors != null && i < dragActors.size) {
+                dragActor = dragActors.get(i);
+            }
+            
+            if (validDragActors != null && i < validDragActors.size) {
+                validDragActor = validDragActors.get(i);
+            }
+            
+            if (invalidDragActors != null && i < invalidDragActors.size) {
+                invalidDragActor = invalidDragActors.get(i);
+            }
+            
+            add(actor, dragActor, validDragActor, invalidDragActor);
+        }
+    }
+    
+    @Override
+    public void clearChildren() {
+        super.clearChildren();
+        actors.clear();
+        dragActors.clear();
+        validDragActors.clear();
+        invalidDragActors.clear();
+    }
+    
+    private void updateTable() {
+        dragAndDrop.clear();
+        for (Button divider : dividers) {
+            removeActor(divider);
+        }
+        dividers.clear();
+        
+        table.clearChildren();
+        for (Actor actor : actors) {
+            if (vertical) table.row();
+            table.add(actor);
+            dragAndDrop.addSource(new Source(actor) {
+                @Override
+                public Payload dragStart(InputEvent event, float x, float y, int pointer) {
+                    actor.setVisible(false);
+                    
+                    for (Button divider : dividers) {
+                        divider.setVisible(true);
+                        divider.setChecked(false);
+                    }
+                    
+                    int index = actors.indexOf(getActor(), true);
+                    if (index == 0) dividers.get(0).setVisible(false);
+                    if (index == actors.size - 1) dividers.peek().setVisible(false);
+                    
+                    Payload payload = new Payload();
+                    payload.setDragActor(dragActors.get(actor));
+                    payload.setValidDragActor(validDragActors.get(actor));
+                    payload.setInvalidDragActor(invalidDragActors.get(actor));
+                    payload.setObject(actor);
+                    return payload;
+                }
+    
+                @Override
+                public void drag(InputEvent event, float x, float y, int pointer) {
+                    for (Button divider : dividers) {
+                        divider.setChecked(false);
+                    }
+                }
+    
+                @Override
+                public void dragStop(InputEvent event, float x, float y, int pointer, Payload payload, Target target) {
+                    actor.setVisible(true);
+    
+                    for (Button divider : dividers) {
+                        divider.setVisible(false);
+                    }
+                }
+            });
+            dragAndDrop.addTarget(new Target(actor) {
+                @Override
+                public boolean drag(Source source, Payload payload, float x, float y, int pointer) {
+                    int index = actors.indexOf(getActor(), true);
+                    if (vertical) {
+                        if (y < getActor().getHeight() / 2) index++;
+                    } else {
+                        if (x > getActor().getWidth() / 2) index++;
+                    }
+                    for (Button divider : dividers) {
+                        divider.setChecked(false);
+                    }
+                    dividers.get(index).setChecked(true);
+                    return true;
+                }
+    
+                @Override
+                public void drop(Source source, Payload payload, float x, float y, int pointer) {
+                    Actor payloadActor = (Actor) payload.getObject();
+                    actors.removeValue(payloadActor, true);
+                    int newIndex = actors.indexOf(getActor(), true);
+                    if (vertical) {
+                        if (y < getActor().getHeight() / 2) newIndex++;
+                    } else {
+                        if (x > getActor().getWidth() / 2) newIndex++;
+                    }
+                    actors.insert(Math.min(newIndex, actors.size), payloadActor);
+                    updateTable();
+                }
+            });
+        }
+        if (vertical) table.row();
+        
+        for (int i = 0; i < actors.size + 1; i++) {
+            Button button = new Button(dividerStyle);
+            button.setVisible(false);
+            addActor(button);
+            dividers.add(button);
+            int index = i;
+            dragAndDrop.addTarget(new Target(button) {
+                @Override
+                public boolean drag(Source source, Payload payload, float x, float y, int pointer) {
+                    return true;
+                }
+    
+                @Override
+                public void drop(Source source, Payload payload, float x, float y, int pointer) {
+                    int newIndex = index;
+                    Actor payloadActor = (Actor) payload.getObject();
+                    int currentIndex = actors.indexOf(payloadActor, true);
+                    if (currentIndex < newIndex) newIndex--;
+                    actors.removeValue(payloadActor, true);
+                    actors.insert(Math.min(newIndex, actors.size), payloadActor);
+                    updateTable();
+                }
+            });
+        }
+    }
+    
+    
+    
+    @Override
+    public float getMinWidth() {
+        return table.getMinWidth();
+    }
+    
+    @Override
+    public float getMinHeight() {
+        return table.getMinHeight();
+    }
+    
+    @Override
+    public float getPrefWidth() {
+        return table.getPrefWidth();
+    }
+    
+    @Override
+    public float getPrefHeight() {
+        return table.getPrefHeight();
+    }
+    
+    @Override
+    public float getMaxWidth() {
+        return table.getMaxWidth();
+    }
+    
+    @Override
+    public float getMaxHeight() {
+        return table.getMaxHeight();
+    }
+    
+    @Override
+    public void layout() {
+        table.setSize(getWidth(), getHeight());
+        table.layout();
+    
+        
+        for (int i = 0; i < dividers.size; i++) {
+            Button button = dividers.get(i);
+            Actor actor;
+            if (vertical) {
+                if (i < actors.size) {
+                    actor = actors.get(i);
+                    button.setPosition(actor.getX(), actor.getY() + actor.getHeight() - button.getHeight() / 2.0f);
+                } else {
+                    actor = actors.get(actors.size - 1);
+                    button.setPosition(actor.getX(), actor.getY() - button.getHeight() / 2.0f);
+                }
+                button.setWidth(actor.getWidth());
+            } else {
+                if (i < actors.size) {
+                    actor = actors.get(i);
+                    button.setPosition(actor.getX() - button.getWidth() / 2.0f, actor.getY());
+                } else {
+                    actor = actors.get(actors.size - 1);
+                    button.setPosition(actor.getX() + actor.getWidth() - button.getWidth() / 2.0f, actor.getY());
+                }
+                button.setHeight(actor.getHeight());
+            }
+            button.layout();
+        }
+    }
+    
+    @Override
+    @Deprecated
+    public void addActor(Actor actor) {
+        super.addActor(actor);
+    }
+    
+    @Override
+    @Deprecated
+    public void addActorAt(int index, Actor actor) {
+        super.addActorAt(index, actor);
+    }
+    
+    @Override
+    @Deprecated
+    public void addActorBefore(Actor actorBefore, Actor actor) {
+        super.addActorBefore(actorBefore, actor);
+    }
+    
+    @Override
+    @Deprecated
+    public void addActorAfter(Actor actorAfter, Actor actor) {
+        super.addActorAfter(actorAfter, actor);
+    }
+    
+    @Override
+    public boolean removeActor(Actor actor, boolean unfocus) {
+        actors.removeValue(actor, true);
+        return super.removeActor(actor, unfocus);
+    }
+    
+    public Table getTable() {
+        return table;
+    }
+    
+    public DraggableListStyle getStyle() {
+        return style;
+    }
+    
+    public static class DraggableListStyle {
+        public Drawable dividerUp, dividerOver;
+        /** Optional **/
+        public Drawable background;
+        
+        public DraggableListStyle() {
+        }
+        
+        public DraggableListStyle(DraggableListStyle style) {
+            background = style.background;
+            dividerUp = style.dividerUp;
+            dividerOver = style.dividerOver;
+        }
+    }
+}
---
+++ core/src/com/ray3k/skincomposer/DraggableTextList.java
@@ -0,0 +1,156 @@
+package com.ray3k.skincomposer;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.Array;
+
+public class DraggableTextList extends DraggableList {
+    private DraggableTextListStyle style;
+    private TextButtonStyle textButtonStyle;
+    private TextButtonStyle dragButtonStyle;
+    private TextButtonStyle validButtonStyle;
+    private TextButtonStyle invalidButtonStyle;
+    private ButtonGroup<TextButton> buttonGroup;
+    
+    public DraggableTextList(boolean vertical, Skin skin) {
+        this(vertical, skin, null);
+    }
+    
+    public DraggableTextList(boolean vertical, Skin skin, String style) {
+        this(vertical, skin.get(style, DraggableTextListStyle.class));
+    }
+    
+    public DraggableTextList(boolean vertical, DraggableTextListStyle style) {
+        super(vertical, style);
+        this.style = style;
+        
+        textButtonStyle = new TextButtonStyle();
+        textButtonStyle.font = style.font;
+        textButtonStyle.up = style.textBackgroundUp;
+        textButtonStyle.over = style.textBackgroundOver;
+        textButtonStyle.down = style.textBackgroundDown;
+        textButtonStyle.checked = style.textBackgroundChecked;
+        textButtonStyle.checkedOver = style.textBackgroundCheckedOver;
+        textButtonStyle.fontColor = style.fontColor;
+        textButtonStyle.overFontColor = style.overFontColor;
+        textButtonStyle.downFontColor = style.downFontColor;
+        textButtonStyle.checkedFontColor = style.checkedFontColor;
+        textButtonStyle.checkedOverFontColor = style.checkedOverFontColor;
+        
+        dragButtonStyle = new TextButtonStyle();
+        dragButtonStyle.font = style.font;
+        dragButtonStyle.up = style.dragBackgroundUp != null ? style.dragBackgroundUp : style.textBackgroundUp;
+        dragButtonStyle.fontColor = style.dragFontColor != null ? style.dragFontColor : style.fontColor;
+        
+        validButtonStyle = new TextButtonStyle();
+        validButtonStyle.font = style.font;
+        validButtonStyle.up = style.validBackgroundUp != null ? style.validBackgroundUp : dragButtonStyle.up;
+        validButtonStyle.fontColor = style.validFontColor != null ? style.validFontColor : dragButtonStyle.fontColor;
+    
+        invalidButtonStyle = new TextButtonStyle();
+        invalidButtonStyle.font = style.font;
+        invalidButtonStyle.up = style.invalidBackgroundUp != null ? style.invalidBackgroundUp : dragButtonStyle.up;
+        invalidButtonStyle.fontColor = style.invalidFontColor != null ? style.invalidFontColor : dragButtonStyle.fontColor;
+        
+        buttonGroup = new ButtonGroup<>();
+    }
+    
+    public void addText(String text) {
+        TextButton actor = new TextButton(text, textButtonStyle);
+        actor.setProgrammaticChangeEvents(false);
+        buttonGroup.add(actor);
+        TextButton dragActor = new TextButton(text, dragButtonStyle);
+        TextButton validDragActor = new TextButton(text, validButtonStyle);
+        TextButton invalidDragActor = new TextButton(text, invalidButtonStyle);
+        super.add(actor, dragActor, validDragActor, invalidDragActor);
+    }
+    
+    public void addAllTexts(Array<String> texts) {
+        for (String text : texts) {
+            addText(text);
+        }
+    }
+    
+    public void addAllTexts(String... texts) {
+        for (String text : texts) {
+            addText(text);
+        }
+    }
+    
+    @Override
+    @Deprecated
+    public void add(Actor actor) {
+        super.add(actor);
+    }
+    
+    @Override
+    @Deprecated
+    public void add(Actor actor, Actor dragActor, Actor validDragActor, Actor invalidDragActor) {
+        super.add(actor, dragActor, validDragActor, invalidDragActor);
+    }
+    
+    @Override
+    @Deprecated
+    public void addAll(Array<Actor> actors) {
+        super.addAll(actors);
+    }
+    
+    @Override
+    @Deprecated
+    public void addAll(Array<Actor> actors, Array<Actor> dragActors, Array<Actor> validDragActors, Array<Actor> invalidDragActors) {
+        super.addAll(actors, dragActors, validDragActors, invalidDragActors);
+    }
+    
+    public CharSequence getSelected() {
+        return buttonGroup.getChecked().getText();
+    }
+    
+    public int getSelectedIndex() {
+        return buttonGroup.getCheckedIndex();
+    }
+    
+    public ButtonGroup getButtonGroup() {
+        return buttonGroup;
+    }
+    
+    public static class DraggableTextListStyle extends DraggableListStyle {
+        public BitmapFont font;
+        /** Optional. */
+        public Drawable textBackgroundUp, textBackgroundDown, textBackgroundOver, textBackgroundChecked,
+                textBackgroundCheckedOver, dragBackgroundUp, validBackgroundUp, invalidBackgroundUp;
+        public Color fontColor, downFontColor, overFontColor, checkedFontColor, checkedOverFontColor, dragFontColor,
+                validFontColor, invalidFontColor;
+    
+        public DraggableTextListStyle() {
+        }
+    
+        public DraggableTextListStyle(DraggableTextListStyle style) {
+            background = style.background;
+            dividerUp = style.dividerUp;
+            dividerOver = style.dividerOver;
+            font = style.font;
+            textBackgroundUp = style.textBackgroundUp;
+            textBackgroundDown = style.textBackgroundDown;
+            textBackgroundOver = style.textBackgroundOver;
+            textBackgroundChecked = style.textBackgroundChecked;
+            textBackgroundCheckedOver = style.textBackgroundCheckedOver;
+            dragBackgroundUp = style.dragBackgroundUp;
+            validBackgroundUp = style.validBackgroundUp;
+            invalidBackgroundUp = style.invalidBackgroundUp;
+            fontColor = style.fontColor;
+            downFontColor = style.downFontColor;
+            overFontColor = style.overFontColor;
+            checkedFontColor = style.checkedFontColor;
+            checkedOverFontColor = style.checkedOverFontColor;
+            dragFontColor = style.dragFontColor;
+            validFontColor = style.validFontColor;
+            invalidFontColor = style.invalidFontColor;
+        }
+    }
+}
